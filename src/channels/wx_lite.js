9ab0204e4f0ff7947e0d4e8d61b7bff8c93d4368
afon <wengxufeng@gmail.com>
2017-01-12 18:58:25 +0800

    更新 README.md

--- src/channels/wx_lite.js
+++ src/channels/wx_lite.js
@@ -3,7 +3,6 @@
  */
 var stash = require('../stash');
 var callbacks = require('../callbacks');
-var mods = require('../mods');
 var hasOwn = {}.hasOwnProperty;
 /*global wx*/
 module.exports = {
@@ -16,71 +15,49 @@ module.exports = {
     for (var k = 0; k < fields.length; k++) {
       if (!hasOwn.call(credential, fields[k])) {
         callbacks.innerCallback('fail',
-            callbacks.error('invalid_credential', 'missing_field_' + fields[k]));
+          callbacks.error('invalid_credential', 'missing_field_' + fields[k]));
         return;
       }
     }
     stash.jsApiParameters = credential;
     this.callpay();
   },
+
   wxLiteEnabled: function () {
     return typeof wx !== 'undefined' && wx.requestPayment;
   },
+
   //微信小程序支付
   callpay: function () {
     if (!this.wxLiteEnabled()) {
-      console.log("请在微信小程序中打开");
+      console.log('请在微信小程序中打开');
       return;
     }
     var wx_lite = stash.jsApiParameters;
     delete wx_lite.appId;
     wx_lite.complete = function (res) {
       //支付成功
-      if (res.errMsg === "requestPayment:ok") {
+      if (res.errMsg === 'requestPayment:ok') {
         callbacks.innerCallback('success');
       }
       //取消支付
-      if (res.errMsg === "requestPayment:cancel") {
-        callbacks.innerCallback('cancel', callbacks.error("用户取消支付"));
+      if (res.errMsg === 'requestPayment:cancel') {
+        callbacks.innerCallback('cancel', callbacks.error('用户取消支付'));
       }
       //支付验证签名失败
       if (res.err_code !== 'undefined' && res.err_desc !== 'undefined') {
-        callbacks.innerCallback('fail', callbacks.error(res.err_desc, res))
+        callbacks.innerCallback('fail', callbacks.error(res.err_desc, res));
       }
     };
     wx.requestPayment(wx_lite);
   },
+
+  /* eslint-disable no-unused-vars */
   runTestMode: function (url) {
     wx.showModal({
       title: '提示',
       content: '因 \"微信小程序\" 限制 域名的原因 暂不支持 模拟付款 请使用 livekey 获取 charge 进行支付'
     });
-    return;
-    wx.showModal({
-      title: '提示',
-      content: '模拟付款?',
-      success: function (res) {
-        if (res.confirm) {
-          console.log(url);
-          wx.request({
-            url: 'https://activity.pingxx.com/demos/base/notfiy',
-            data: {url: url},
-            fail: function (res) {
-            },
-            complete: function (res) {
-              var status = res.statusCode
-              if (status >= 200 && status < 400 && res.data == 'success') {
-                callbacks.innerCallback('success');
-              } else {
-                var extra = 'http_code:' + status + ';response:' + res.data;
-                callbacks.innerCallback('fail',
-                    callbacks.error('testmode_notify_fail', extra));
-              }
-            }
-          });
-
-        }
-      }
-    });
   }
+  /* eslint-enable no-unused-vars */
 };
